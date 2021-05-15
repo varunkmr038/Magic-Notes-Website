11825cc9b35f756e5153d22e6830b7fffe831ec3
varun <varunkmr038@gmail.com>
2021-05-15 22:53:57 +0530

    fixed imp feature

--- script.js
+++ script.js
@@ -1,40 +1,33 @@
-let addbtn = document.getElementById('addbtn');
-var impcount=0;
+let addbtn = document.getElementById("addbtn");
+var impcount = 0;
 
-
-if(localStorage.length!=0)
-update(2);
+if (localStorage.length != 0) update(2);
 
 addbtn.addEventListener("click", function () {
-    update(1);
+  update(1);
 });
-let clearbtn = document.getElementById('clearbtn');
+
+let clearbtn = document.getElementById("clearbtn");
 clearbtn.addEventListener("click", clearAll);
 
 function update(flag) {
+  var notes = document.getElementById("notes");
+  let notestring = localStorage.getItem("notes");
 
-     
-
-    var notes = document.getElementById('notes');
-    let notestring = localStorage.getItem('notes');
-    if (flag == 1) {
-        Alert();
-        var addtxt = document.getElementById('addtxt');
-        var title=document.getElementById('title');
-        if (notestring == null)
-            notesarr = [];
-
-        else notesarr = JSON.parse(notestring);
-        notesarr.push([addtxt.value, 'card-body',title.value]);
-        addtxt.value = ``;
-        title.value=``;
-        localStorage.setItem('notes', JSON.stringify(notesarr));
-
-    }
+  if (flag == 1) {
+    Alert();
+    var addtxt = document.getElementById("addtxt");
+    var title = document.getElementById("title");
+    if (notestring == null) notesarr = [];
     else notesarr = JSON.parse(notestring);
-    str = ``;
-    notesarr.forEach((element, index) => {
-        str += `
+    notesarr.push([addtxt.value, "card-body", title.value]);
+    addtxt.value = ``;
+    title.value = ``;
+    localStorage.setItem("notes", JSON.stringify(notesarr));
+  } else notesarr = JSON.parse(notestring);
+  str = ``;
+  notesarr.forEach((element, index) => {
+    str += `
  <div class="mx-2 my-3" style="width: 18rem;">
     <div class="${element[1]}">
       <h5 class="card-title">${element[2]}</h5>
@@ -44,44 +37,39 @@ function update(flag) {
     </div>
   </div>
  `;
+  });
 
-    });
-
-    notes.innerHTML = str;
+  notes.innerHTML = str;
 }
 
 function deleted(index) {
-    let notestring = localStorage.getItem('notes');
-    notesarr = JSON.parse(notestring);
+  let notestring = localStorage.getItem("notes");
+  notesarr = JSON.parse(notestring);
 
-    notesarr.splice(index, 1);
-    localStorage.setItem('notes', JSON.stringify(notesarr));
+  notesarr.splice(index, 1);
+  localStorage.setItem("notes", JSON.stringify(notesarr));
 
-    update(2);
-
-
-
+  update(2);
 }
 
 function clearAll() {
-    if (confirm("Do you really want to clear ?")) {
-        console.log("Clearing List");
-        localStorage.clear();
-        notes.innerHTML = "";
-    }
-
+  if (confirm("Do you really want to clear ?")) {
+    console.log("Clearing List");
+    localStorage.clear();
+    notes.innerHTML = "";
+  }
 }
 
-let search = document.getElementById('searchtxt');
+let search = document.getElementById("searchtxt");
 
-search.addEventListener('input', function () {
-    searchtxt = search.value.toLowerCase();
-    let notestring = localStorage.getItem('notes');
-    notesarr = JSON.parse(notestring);
-    str = ``;
-    notesarr.forEach((element, index) => {
-        if (element[0].includes(searchtxt)) {
-            str += `
+search.addEventListener("input", function () {
+  searchtxt = search.value.toLowerCase();
+  let notestring = localStorage.getItem("notes");
+  notesarr = JSON.parse(notestring);
+  str = ``;
+  notesarr.forEach((element, index) => {
+    if (element[0].includes(searchtxt)) {
+      str += `
         <div class="mx-2 my-3" style="width: 18rem;">
            <div class="${element[1]}">
              <h5 class="card-title">${element[2]}</h5>
@@ -91,82 +79,72 @@ search.addEventListener('input', function () {
            </div>
          </div>
         `;
-        }
+    }
 
-        notes.innerHTML = str;
-    });
-
+    notes.innerHTML = str;
+  });
 });
 
-
 function markimp(number) {
+  let notestring = localStorage.getItem("notes");
+  notesarr = JSON.parse(notestring);
 
-    let notestring = localStorage.getItem('notes');
-    notesarr = JSON.parse(notestring);
+  notesarr.forEach((element, index) => {
+    if (index == number) {
+      if (element[1] == "imp card-body") element[1] = "card-body";
+      else element[1] = "imp card-body";
+    }
+  });
+  localStorage.setItem("notes", JSON.stringify(notesarr));
 
-    notesarr.forEach((element, index) => {
-        if (index == number) {
-
-            if (element[1] == 'imp card-body')
-                element[1] = 'card-body';
-
-            else element[1] = 'imp card-body';
-
-        }
-
-    });
-    localStorage.setItem('notes', JSON.stringify(notesarr));
-
-    update(2);
+  update(2);
 }
 
-impNotesbtn = document.getElementById('impNotesbtn');
+impNotesbtn = document.getElementById("impNotesbtn");
 
 impNotesbtn.addEventListener("click", function () {
-      if(impcount%2==0){
-    let notesarr = document.getElementsByClassName('imp card-body');
-      str = ``;
-    Array.from(notesarr).forEach(element => {
-        wrap=document.createElement('div');
-         wrap.appendChild(element.parentElement);
-        str += `
+  impcount++;
+  if (impcount % 2 != 0) {
+    let notesarr = document.getElementsByClassName("imp card-body");
+    str = ``;
+    Array.from(notesarr).forEach((element) => {
+      wrap = document.createElement("div");
+      wrap.appendChild(element.parentElement);
+      str += `
     ${wrap.innerHTML}
     `;
     });
     notes.innerHTML = str;
-    impcount++;
-}
-else {
-    impcount++;
+  } else {
     update(2);
-}
+  }
 });
 
-var resetbtn=document.getElementById('resetbtn');
+var resetbtn = document.getElementById("resetbtn");
 
-resetbtn.addEventListener("click",function(){
-    let notestring = localStorage.getItem('notes');
-    notesarr = JSON.parse(notestring);
+resetbtn.addEventListener("click", function () {
+  let notestring = localStorage.getItem("notes");
+  notesarr = JSON.parse(notestring);
 
-    notesarr.forEach((element, index) => {
+  notesarr.forEach((element, index) => {
+    element[1] = "card-body";
+    0;
+  });
+  localStorage.setItem("notes", JSON.stringify(notesarr));
 
-                element[1] = 'card-body';
+  update(2);
 });
-    localStorage.setItem('notes', JSON.stringify(notesarr));
 
-    update(2);
-});
+function Alert() {
+  alert = document.getElementById("alert");
 
-function Alert(){
-alert=document.getElementById('alert');
-
-alert.innerHTML=`
+  alert.innerHTML = `
 <div class="alert alert-info alert-dismissible fade show" role="alert">
   <strong>Woohooo!!!</strong> Your Notes has been added
   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
 `;
-setTimeout(function(){
-alert.innerHTML=``;
-},3000);
+  setTimeout(function () {
+    alert.innerHTML = ``;
+  }, 3000);
 }
